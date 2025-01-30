8d32e15ed2efa6052f818a9874366f5cbe47ad24
tnk-da <tanakad@s-p-net.com>
2025-01-30 18:32:47 +0900

    コード修正

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -12,7 +12,7 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps = async ({params}) => {
-  const { post: post } = await getPostData(params.id);
+  const post = await getPostData(params.id);
   return {
     props: { post }
   }
